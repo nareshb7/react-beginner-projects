c3ea3fb393a580f3c05ea45b76a5d1e373c4f5b0
nareshb7 <104125516+nareshb7@users.noreply.github.com>
2023-01-30 22:48:17 +0530

    Footer Contact Adrs NavLinks added

--- src/components/footer/ImportantLinks.js
+++ src/components/footer/ImportantLinks.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const ImportantLinks = () => {
   return (
     <div style={{ marginBlock: '10px' }}>
       <h3 style={{ marginBlock: '5px' }}>Important Links :</h3>
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Contact Us</li>
+        <li>
+          <NavLink to="/">Home</NavLink>{' '}
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>{' '}
+        </li>
+        <li>
+          <NavLink to="/projects">Projects</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">Contact Us</NavLink>{' '}
+        </li>
       </ul>
     </div>
   );
