b22d53da7da981fe0506ee49943b36e2de8fc0cd
nareshb7 <104125516+nareshb7@users.noreply.github.com>
2023-01-31 15:39:49 +0530

    home > carousel >  imgBinary Comp added

--- src/components/home/Carousel.js
+++ src/components/home/Carousel.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const Carousel =()=> {
+const ImgStore = () => {
+  const prevImage = JSON.parse(localStorage.getItem('binaryData') || '');
+  const [img, setImg] = useState(prevImage);
+  const convertToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+    const base64 = await convertToBase64(file);
+    console.log(base64, 'base64');
+    setImg(base64);
+  };
+  const submitFunc = () => {
+    localStorage.setItem('binaryData', JSON.stringify(img));
+  };
   return (
-    <div>Carousel</div>
-  )
-}
-export default Carousel
+    <div>
+      <div>
+        <h4>Upload Image : </h4>
+        <input type="file" name="image" onChange={handleChange} />
+        <button onClick={submitFunc}>Submit</button>
+      </div>
+      <div>
+        <img
+          src={img}
+          alt="imageee"
+          style={{ width: '300px', height: '300px' }}
+        />
+      </div>
+    </div>
+  );
+};
+export default ImgStore;
